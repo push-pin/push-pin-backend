86f425817e5df521f0badceb8885ee3c731030f6
UncleJerry <jared.myhrberg@gmail.com>
2019-06-12 11:18:15 -0700

    submission test

--- tests/models/Submission.test.js
+++ tests/models/Submission.test.js
@@ -4,20 +4,16 @@ const mongoose = require('mongoose');
 describe('Submission model', () => {
   it('new Submission', () => {
     const submission = new Submission({
+      assignment: new mongoose.Types.ObjectId,
       submission: 'github.com',
-      grade: null,
-      gradeComment: '',
-      graded: false,
-      gradedBy: new mongoose.Types.ObjectId
+      graded: false
     });
 
     expect(submission.toJSON()).toEqual({
       _id: expect.any(mongoose.Types.ObjectId),
+      assignment: expect.any(mongoose.Types.ObjectId),
       submission: 'github.com',
-      grade: null,
-      gradeComment: '',
-      graded: false,
-      gradedBy: expect.any(mongoose.Types.ObjectId)
+      graded: false
     });
   });
 });
