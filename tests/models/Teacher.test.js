6f6b4f7a1b7f49e18b6e38e45a98c42698994a49
UncleJerry <jared.myhrberg@gmail.com>
2019-06-12 11:47:43 -0700

    teacher test

--- tests/models/Teacher.test.js
+++ tests/models/Teacher.test.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 describe('Teacher model', () => {
   it('new teacher', () => {
     const teacher = new Teacher({
-      user: '1234567890987654321',
+      auth0Id: '1234567890987654321',
+      classes: [new mongoose.Types.ObjectId, new mongoose.Types.ObjectId]
     });
 
     expect(teacher.toJSON()).toEqual({
       _id: expect.any(mongoose.Types.ObjectId),
-      user: expect.any(String)
+      auth0Id: expect.any(String),
+      classes: [expect.any(mongoose.Types.ObjectId), expect.any(mongoose.Types.ObjectId)]
     });
   });
 });
