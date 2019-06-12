02dd1c00b6587d33113fe90063b4f25d31b55250
UncleJerry <jared.myhrberg@gmail.com>
2019-06-12 12:54:48 -0700

    teacherAssistant

---
+++ tests/models/TeacherAssistant.test.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const TeacherAssistant = require('../../lib/models/TeacherAssistant');
+
+describe('TeacherAssistant model', () => {
+  it('new TeacherAssistant', () => {
+    const teacherAssistant = new TeacherAssistant({
+      auth0ID: '1234567890',
+      currentClass: new mongoose.Types.ObjectId,
+      pastClasses: [new mongoose.Types.ObjectId, new mongoose.Types.ObjectId]
+    });
+
+    expect(teacherAssistant.toJSON()).toEqual({
+      _id: expect.any(mongoose.Types.ObjectId),
+      auth0ID: '1234567890',
+      currentClass: expect.any(mongoose.Types.ObjectId),
+      pastClasses: [expect.any(mongoose.Types.ObjectId), expect.any(mongoose.Types.ObjectId)]
+    });
+  });
+});
