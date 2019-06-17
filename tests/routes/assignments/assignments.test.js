695b38ad5588979673322ea600835622b64e933a
mcnadams <mcneilbo@oregonstate.edu>
2019-06-17 15:01:08 -0700

    assignments routes testing

--- tests/routes/assignments/assignments.test.js
+++ tests/routes/assignments/assignments.test.js
@@ -65,7 +65,103 @@ describe('assignment route tests', () => {
           active: true
         });
       });
+  });
 
+  it('gets all archived assignments for a course', async() => {
+    const course = await Course.find();
+    await Assignment.create({
+      course: course[0]._id,
+      type: 'reading',
+      title: 'Read this thing',
+      instructions: 'Read pages 1-10 and answer the questions',
+      dateAvailable: new Date(),
+      dateDue: new Date(),
+      dateClosed: new Date(),
+      active: false
+    });
+    await Assignment.create({
+      course: course[0]._id,
+      type: 'solo',
+      title: 'Do this thing',
+      instructions: 'fork the repo and do the lab',
+      dateAvailable: new Date(),
+      dateDue: new Date(),
+      dateClosed: new Date(),
+      active: false
+    });
+    return request(app)
+      .get(`/api/v1/assignments/course/inactive/${course[0]._id}`)
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+        expect(res.body[0]).toEqual({
+          course: expect.any(String),
+          _id: expect.any(String),
+          type: expect.any(String),
+          title: expect.any(String),
+          instructions: expect.any(String),
+          dateAvailable: expect.any(String),
+          dateDue: expect.any(String),
+          dateClosed: expect.any(String),
+          active: false
+        });
+      });
+  });
+
+  it('gets an assignment by id', async() => {
+    const ass = await Assignment.create({
+      course: new mongoose.Types.ObjectId,
+      type: 'reading',
+      title: 'Read this thing',
+      instructions: 'Read pages 1-10 and answer the questions',
+      dateAvailable: new Date(),
+      dateDue: new Date(),
+      dateClosed: new Date()
+    });
+    return request(app)
+      .get(`/api/v1/assignments/${ass._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          active: true,
+          course: expect.any(String),
+          type: 'reading',
+          title: 'Read this thing',
+          instructions: 'Read pages 1-10 and answer the questions',
+          dateAvailable: expect.any(String),
+          dateDue: expect.any(String),
+          dateClosed: expect.any(String),
+        });
+      });
+  });
+
+  it('updates an assignment', async() => {
+    const ass = await Assignment.create({
+      course: new mongoose.Types.ObjectId,
+      type: 'reading',
+      title: 'Read this thing',
+      instructions: 'Read pages 1-10 and answer the questions',
+      dateAvailable: new Date(),
+      dateDue: new Date(),
+      dateClosed: new Date()
+    });
+    return request(app)
+      .patch(`/api/v1/assignments/${ass._id}`)
+      .send({
+        title: 'Hello there!'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          course: expect.any(String),
+          _id: expect.any(String),
+          type: 'reading',
+          title: 'Hello there!',
+          instructions: 'Read pages 1-10 and answer the questions',
+          dateAvailable: expect.any(String),
+          dateDue: expect.any(String),
+          dateClosed: expect.any(String),
+          active: true
+        });
+      });
   });
 
 });
