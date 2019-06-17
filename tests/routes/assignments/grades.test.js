8c0b1abe022d10b4fe046a8ae8ffda9f800da838
mcnadams <mcneilbo@oregonstate.edu>
2019-06-17 16:19:46 -0700

    grades route test

--- tests/routes/assignments/grades.test.js
+++ tests/routes/assignments/grades.test.js
@@ -3,14 +3,38 @@ const request = require('supertest');
 const app = require('../../../lib/app');
 const mongoose = require('mongoose');
 const connect = require('../../../lib/utils/connect');
-const { seedAsses } = require('../../utils/seed-data');
-const Assignment = require('../../../lib/models/assignments/Assignment');
-const Course = require('../../../lib/models/Course');
+const { seedGrades } = require('../../utils/seed-data');
+const Grade = require('../../../lib/models/assignments/Grade');
+
+jest.mock('../../../lib/middleware/ensure-auth.js');
+
+beforeAll(() => connect());
+
+beforeEach(() => mongoose.connection.dropDatabase());
+beforeEach(async() => {
+  return await Promise.all([seedGrades()]);
+});
+
+afterAll(() => mongoose.connection.close());
 
 describe('grade route tests', () => {
 
-  it('is a dummy test', () => {
-
-  });
+  it('posts a new grade', () => {
+    return request(app)
+      .post('/api/v1/grades/')
+      .send({
+        submission: new mongoose.Types.ObjectId,
+        grade: 98,
+        grader: new mongoose.Types.ObjectId
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          submission: expect.any(String),
+          grader: expect.any(String),
+          grade: 98
+        });
+      });
+  }); 
 
 });
