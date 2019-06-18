ba4bb354b9c7b69cb7eb87e563e5d5bb1ab426db
mcnadams <mcneilbo@oregonstate.edu>
2019-06-18 10:41:57 -0700

    get most recent announcements

--- tests/routes/announcements.test.js
+++ tests/routes/announcements.test.js
@@ -3,12 +3,15 @@ const request = require('supertest');
 const app = require('../../lib/app');
 const mongoose = require('mongoose');
 const connect = require('../../lib/utils/connect');
+const { seedAnnouncements } = require('../utils/seed-data');
+const Course = require('../../lib/models/Course');
 
 jest.mock('../../lib/middleware/ensure-auth.js');
 
 beforeAll(() => connect());
 
 beforeEach(() => mongoose.connection.dropDatabase());
+beforeEach(async() => await Promise.all([seedAnnouncements()]));
 
 afterAll(() => mongoose.connection.close());
 
@@ -34,6 +37,25 @@ describe('announcement route tests', () => {
           updatedAt: expect.any(String),
           createdAt: expect.any(String)
         });
+      });
+  });
+
+  it('gets 20 most recent active announcements', async() => {
+    const course = await Course.findOne();
+    return request(app)
+      .get(`/api/v1/announcements/${course._id}`)
+      .then(res => {
+        expect(res.body).toHaveLength(20);
+        expect(res.body[0]).toEqual({
+          user: expect.any(Object),
+          _id: expect.any(String),
+          course: expect.any(String),
+          active: true,
+          title: expect.any(String),
+          body: expect.any(String),
+          updatedAt: expect.any(String),
+          createdAt: expect.any(String)
+        })
       });
   });
 
