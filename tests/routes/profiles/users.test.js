aac9aa275dd23fb3c076b734108ac8d71e507fe1
mcnadams <mcneilbo@oregonstate.edu>
2019-06-19 16:37:41 -0700

    get more info from auth0id route

--- tests/routes/profiles/users.test.js
+++ tests/routes/profiles/users.test.js
@@ -4,9 +4,11 @@ const app = require('../../../lib/app');
 const mongoose = require('mongoose');
 const connect = require('../../../lib/utils/connect');
 const User = require('../../../lib/models/profiles/User');
-// const { 
-//   seedStudents
-// } = require('../../utils/seed-data');
+const { 
+  seedStudents,
+  seedTAs, 
+  seedTeachers
+} = require('../../utils/seed-data');
 
 jest.mock('../../../lib/middleware/ensure-auth.js');
 
@@ -14,35 +16,77 @@ beforeAll(() => connect());
 
 beforeEach(() => mongoose.connection.dropDatabase());
 // beforeEach(async() => {
-//   return await Promise.all([seedStudents()]);
+//   return await Promise.all([seedTAs()]);
 // });
 
 afterAll(() => mongoose.connection.close());
 
 describe('user route tests', () => {
 
-  it('gets a user by auth0id', async() => {
-    const user = await User.create({
-      auth0id: '12345beep',
-      firstName: 'Ryan',
-      lastName: 'Gosling',
-      email: 'ryan@gosling.com',
-      role: 'teacher'
-    });
+  it('gets a user who is a ta by auth0id', async() => {
+    await seedTAs();
+    const user = await User.findOne();
     return request(app)
       .get(`/api/v1/user/auth/${user.auth0id}`)
       .then(res => {
         expect(res.body).toEqual({
-          auth0id: '12345beep',
-          firstName: 'Ryan',
-          lastName: 'Gosling',
-          email: 'ryan@gosling.com',
-          role: 'teacher',
-          _id: expect.any(String),
-          image: '../..//assets/placeholder.png'
+          user: {
+            auth0id: expect.any(String),
+            firstName: expect.any(String),
+            lastName: expect.any(String),
+            email: expect.any(String),
+            role: 'ta',
+            _id: expect.any(String),
+            image: '../..//assets/placeholder.png',
+          },
+          profile: expect.any(Object),
+          currentCourses: expect.any(Object)
         });
       });
+  });
 
+  it('gets a user who is a student by auth0id', async() => {
+    await seedStudents();
+    const user = await User.findOne();
+    return request(app)
+      .get(`/api/v1/user/auth/${user.auth0id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            auth0id: expect.any(String),
+            firstName: expect.any(String),
+            lastName: expect.any(String),
+            email: expect.any(String),
+            role: 'student',
+            _id: expect.any(String),
+            image: '../..//assets/placeholder.png',
+          },
+          profile: expect.any(Object),
+          currentCourses: expect.any(Object)
+        });
+      });
+  });
+
+  it('gets a user who is a teacher by auth0id', async() => {
+    await seedTeachers();
+    const user = await User.findOne();
+    return request(app)
+      .get(`/api/v1/user/auth/${user.auth0id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            auth0id: expect.any(String),
+            firstName: expect.any(String),
+            lastName: expect.any(String),
+            email: expect.any(String),
+            role: 'teacher',
+            _id: expect.any(String),
+            image: '../..//assets/placeholder.png',
+          },
+          profile: expect.any(Object),
+          currentCourses: expect.any(Array)
+        });
+      });
   });
 
 });
