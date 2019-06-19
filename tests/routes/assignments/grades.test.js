d2deffc9f4914941991c803073918eccbaa2d83e
UncleJerry <jared.myhrberg@gmail.com>
2019-06-19 09:56:18 -0700

    total grade for student working

--- tests/routes/assignments/grades.test.js
+++ tests/routes/assignments/grades.test.js
@@ -4,8 +4,11 @@ const app = require('../../../lib/app');
 const mongoose = require('mongoose');
 const connect = require('../../../lib/utils/connect');
 const { seedGrades } = require('../../utils/seed-data');
+const chance = require('chance').Chance();
 const Grade = require('../../../lib/models/assignments/Grade');
 const Submission = require('../../../lib/models/assignments/Submission');
+const User = require('../../../lib/models/profiles/User');
+const Assignment = require('../../../lib/models/assignments/Assignment');
 
 jest.mock('../../../lib/middleware/ensure-auth.js');
 
@@ -47,6 +50,47 @@ describe('grade route tests', () => {
           _id: expect.any(String),
           submission: expect.any(String),
           grader: expect.any(String),
+          grade: expect.any(Number)
+        });
+      });
+  });
+
+  it('gets total grade for student', async() => {
+    const ass = await Assignment.findOne();
+    const user = await User.create({
+      auth0id: chance.word(),
+      firstName: 'first',
+      lastName: 'last',
+      email: chance.email(),
+      role: 'student'
+    });
+    const sub = await Submission.create({
+      assignment: ass._id,
+      student: user._id,
+      submission: 'heres my thing'
+    });
+    // eslint-disable-next-line no-unused-vars
+    const grade = await Grade.create({
+      submission: sub._id,
+      grade: 98,
+      grader: new mongoose.Types.ObjectId
+    });
+    // eslint-disable-next-line no-unused-vars
+    const sub1 = await Submission.create({
+      assignment: ass._id,
+      student: user._id,
+      submission: 'heres my thing'
+    });
+    // eslint-disable-next-line no-unused-vars
+    const grade1 = await Grade.create({
+      submission: sub._id,
+      grade: 42,
+      grader: new mongoose.Types.ObjectId
+    });
+    return request(app)
+      .get(`/api/v1/grades/student/${user._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
           grade: expect.any(Number)
         });
       });
